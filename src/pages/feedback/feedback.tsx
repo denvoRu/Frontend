f8968835e337c25286cb5423730af0e32914f3e4
artemmel401 <125571136+artemmel401@users.noreply.github.com>
2025-01-11 18:53:36 +0500

    fix build bug

--- src/pages/feedback/feedback.tsx
+++ src/pages/feedback/feedback.tsx
@@ -1,9 +1,5 @@
-import { useParams } from 'react-router-dom'
-import styles from './feedback.module.scss'
 
 export default function FeedBack(){
-
-  const {id} = useParams()
 
   return (
     <div>
